bf8c98c0dfa9e0e49c906cf23f889f0959d93099
Connor Cherry <Connor18980@gmail.com>
2022-09-19 18:44:11 -0400

    Main Menu Change
    Change UI on main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,19 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    
+    [SerializeField] private Slider volumeChanger;
+    [SerializeField] private GameObject noSound;
+    [SerializeField] private GameObject sound;
     void Start()
     {
-        
+        volumeChanger.value = 50;
     }
 
     
     void Update()
     {
-        
+        if (volumeChanger.value == 0)
+        {
+            sound.SetActive(false);
+            noSound.SetActive(true);
+        }
+        else
+        {
+            noSound.SetActive(false);
+            sound.SetActive(true);
+        }
     }
 
     public void OnClickQuitButton()
