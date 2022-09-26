f80fd76a4fba2efc060323d82b1449480189d0a5
alexbeifeld <91287688+alexbeifeld@users.noreply.github.com>
2022-09-26 17:19:11 -0400

    Terrain hazard start

---
+++ Assets/Scripts/Grass.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+    [SerializeField] private GameObject treePrefab;
+
+    [SerializeField] private List<Vector3> spawnLocations = new List<Vector3>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
